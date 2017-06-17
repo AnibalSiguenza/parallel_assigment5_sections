72bd63df605ba5ce23341982694b9f8b313e0734
Anibal <anibal.siguenza1@gmail.com>
2017-06-17 15:15:08 +0200

    Accepted version =)

--- student/companytree_par.c
+++ student/companytree_par.c
@@ -1,32 +1,26 @@
 #include "companytree.h"
 #include <omp.h>
-
-void parallelTranverse(tree *node, int n){
-    if(node != NULL){
-        node->work_hours = compute_workHours(node->data);
-        top_work_hours[node->id] = node->work_hours;
-        #pragma omp sections
-        {
-            #pragma omp section 
-            {
-                parallelTranverse(node->right, n-1);
-            }
-            #pragma omp section
-            {
-                parallelTranverse(node->left, n-1);
-            } 
-
-        }
-    }
-}
+#define T 4
 
 void traverse(tree *node, int numThreads){
     omp_set_nested(1);
-    omp_set_max_active_levels(8);
-#pragma omp parallel num_threads(numThreads)
-    {
-#pragma omp single
-        parallelTranverse(node,numThreads);
+    omp_set_max_active_levels(T);
+    omp_set_num_threads(numThreads);
+    if(node != NULL){
+        node->work_hours = compute_workHours(node->data);
+        top_work_hours[node->id] = node->work_hours;
+        #pragma omp parallel sections
+        {
+            #pragma omp section 
+            {
+                traverse(node->right, numThreads);
+            }
+            #pragma omp section
+            {
+                traverse(node->left, numThreads);
+            } 
+
+        }
     }
 }
 
